66f752ba61b08db1fbb35837953c18c3ce7de65f
dotzipfile <davenickmalone@gmail.com>
2018-03-15 01:01:45 +0000

    added some comments

--- src/recursion/Euclid.java
+++ src/recursion/Euclid.java
@@ -3,9 +3,11 @@ package recursion;
 public class Euclid {
 
 	public int gcd(int p, int q) {
+		// If q is 0, p is the greatest common divisor 
 		if(q == 0) {
 			return p;
 		} else {
+			// Call function again
 			return gcd(q, p % q);
 		}
 	}
